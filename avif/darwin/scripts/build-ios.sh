73195692ca6f267ab9ae9f9a9bf9709c157f45a7
Luca Spinazzola <mspinluca@gmail.com>
2025-11-09 22:35:28 -0500

    fix ios simulator AVIF_LOCAL_LIBSHARPYUV=ON and avif target mapping

--- avif/darwin/scripts/build-ios.sh
+++ avif/darwin/scripts/build-ios.sh
@@ -30,6 +30,39 @@ cd ..
 cd ../..
 # END dav1d
 
+# START libsharpyuv (from libwebp)
+if ! [ -d ext/libwebp ]; then
+  echo "Error: ext/libwebp directory not found"
+  exit 255
+fi
+
+cd ext/libwebp || exit 255
+
+libsharpyuv_build_dir="build"
+rm -rf "${libsharpyuv_build_dir}"
+mkdir -p "${libsharpyuv_build_dir}"
+
+cmake -B "${libsharpyuv_build_dir}" -G Xcode ${IOS_CMAKE_PARAMS} \
+  -DCMAKE_BUILD_TYPE=Release \
+  -DBUILD_SHARED_LIBS=OFF \
+  -DWEBP_BUILD_ANIM_UTILS=OFF \
+  -DWEBP_BUILD_CWEBP=OFF \
+  -DWEBP_BUILD_DWEBP=OFF \
+  -DWEBP_BUILD_GIF2WEBP=OFF \
+  -DWEBP_BUILD_IMG2WEBP=OFF \
+  -DWEBP_BUILD_VWEBP=OFF \
+  -DWEBP_BUILD_WEBPINFO=OFF \
+  -DWEBP_BUILD_WEBPMUX=OFF \
+  -DWEBP_BUILD_EXTRAS=OFF
+
+cmake --build "${libsharpyuv_build_dir}" --config Release --target sharpyuv
+
+# Copy libsharpyuv.a to the expected location
+cp -v "${libsharpyuv_build_dir}/Release-"*"/libsharpyuv.a" "${libsharpyuv_build_dir}/" || exit 255
+
+cd ../..
+# END libsharpyuv
+
 # START avif
 build_dir="_build-ios_${ARCH}"
 rm -rf "${build_dir}"
@@ -42,7 +75,7 @@ cmake -B "${build_dir}" -G Xcode \
   -DAVIF_CODEC_DAV1D=ON \
   -DAVIF_LOCAL_DAV1D=ON \
   -DAVIF_LOCAL_LIBYUV=OFF \
-  -DAVIF_LOCAL_LIBSHARPYUV=OFF
+  -DAVIF_LOCAL_LIBSHARPYUV=ON
 cmake --build "${build_dir}" --config Release
 # END avif
 
@@ -53,5 +86,6 @@ cp -v ext/dav1d/build/src/*.a "${IOS_OUTPUT_DIR}" || exit 255
 cp -v ${build_dir}/Release-*/*.a "${IOS_OUTPUT_DIR}" || exit 255
 
 rm -rf "ext/dav1d/build"
+rm -rf "ext/libwebp/build"
 rm -rf "${build_dir}"
 # END copy *.a & rm cache dir
